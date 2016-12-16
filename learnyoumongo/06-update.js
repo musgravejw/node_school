e5644ce634064296eda0fad190810a9f225be013
musgravejw <musgravejw@gmail.com>
2016-12-15 20:58:18 -0500

    06-update.js

---
+++ learnyoumongo/06-update.js
@@ -0,0 +1,16 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/' + process.argv[2];
+
+mongo.connect(url, function(err, db) {
+  var collection = db.collection('users');
+  
+  collection.update({
+    username: 'tinatime'
+  }, {
+    $set: {
+      age: 40
+    }
+  }, function(err) {
+    db.close()
+  });
+});
